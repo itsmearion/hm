94d64a51ad1fd238106a6d1e06d80f9ed8a6fac1
Lampurge <bilqiss0893@gmail.com>
2025-05-09 15:17:43 +0700

    Create format_menu.py

---
+++ handlers/format_menu.py
@@ -0,0 +1,31 @@
+FORMAT_LIST = {
+    "nokos": {
+        "label": "Nokos Telegram",
+        "text": """   ⸼  ۫ format.zip
+
+˖  ˚ ₊ username + nama : 
+˖  ˚ ₊ jenis orderan : nokos telegram 
+˖  ˚ ₊ fresh/nonfresh :
+˖  ˚ ₊ jumlah order : 
+˖  ˚ ₊ total : 
+˖  ˚ ₊ admin handle :
+˖  ˚ ₊ link BBC catalogue : (jika tidak ada kosongin aja )
+˖  ˚ ₊ payment : 
+˖  ˚ ₊ rekber? : yay/nay 
+˖  ˚ ₊ sudah subscribe @mourpals : yes/no"""
+    },
+    "robux": {
+        "label": "Robux Vilog",
+        "text": """   ⸼  ۫ format.zip
+
+˖  ˚ ₊ username + nama : 
+˖  ˚ ₊ jenis orderan : robux login/premium 
+˖  ˚ ₊ jumlah order : 
+˖  ˚ ₊ total : 
+˖  ˚ ₊ admin handle :
+˖  ˚ ₊ link BBC catalogue : (jika tidak ada kosongin aja )
+˖  ˚ ₊ payment : 
+˖  ˚ ₊ rekber? : yay/nay 
+˖  ˚ ₊ sudah subscribe @mourpals : yes/no"""
+    }
+}
